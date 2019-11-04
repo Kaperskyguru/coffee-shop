511686c79bc2dee939c44b1b5c29788d3a20902b
kaperskyguru <solomoneseme@gmail.com>
2019-11-04 16:59:05 +0100

    Fix for final production

---
+++ components/navigation/Menu.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-toolbar
+    dense
+    floating
+    flat
+    class="menu"
+  >
+    <v-toolbar-side-icon>
+      <Logo />
+    </v-toolbar-side-icon>
+    <v-spacer />
+    <v-toolbar-items class="menu-items">
+      <v-btn text>
+        SHOP
+      </v-btn>
+      <v-btn text>
+        WHOLESALE
+      </v-btn>
+      <v-btn text>
+        LOCATIONS
+      </v-btn>
+      <v-btn text>
+        OUR STORY
+      </v-btn>
+    </v-toolbar-items>
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+import Logo from '~/components/Logo'
+export default {
+  name: 'Menu',
+
+  components: {
+    Logo
+  }
+}
+</script>
+<style scoped>
+.menu-items button{
+  font-weight: 400;
+    font-size: .6rem;
+    text-transform: uppercase;
+}
+.menu{
+  display: inline;
+  z-index: -1
+}
+</style>
---
+++ components/navigation/Nav.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-row class="menu">
+    <v-col
+      :lg="12"
+      :md="12"
+      :align-self="right"
+      justify-self="end"
+    >
+      <Menu />
+    </v-col>
+    <v-row>
+      <v-col
+        lg="6"
+        md="6"
+        sm="6"
+      >
+        <CoffeeText :title="title" class="mb-10" />
+        <Button width="180" />
+      </v-col>
+      <v-col
+        md="6"
+        lg="6"
+        :align-self="start"
+        class="hidden-sm-and-down"
+      >
+        <v-img src="/coffeebeans.png" class="coffeeimg" />
+      </v-col>
+    </v-row>
+  </v-row>
+</template>
+
+<script>
+import Menu from '~/components/navigation/Menu'
+import CoffeeText from '~/components/texts/CoffeeText'
+import Button from '~/components/buttons/Button'
+export default {
+  components: {
+    Menu,
+    CoffeeText,
+    Button
+  },
+  data () {
+    return {
+      title: 'A cup of coffee is one of the most important, simple pleasures in life.'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.menu{
+position: relative;
+z-index: 1;
+}
+
+ .coffeeimg {
+  max-width: 400px !important;
+  /* max-height: 400px !important; */
+   /* left: unset; */
+   position: relative;
+}
+
+</style>
